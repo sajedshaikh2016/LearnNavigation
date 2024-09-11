94483d6517fe99c693ddd6f47b6c21e8363d0930
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-09-11 18:03:12 +0530

    Initial Commit

---
+++ LearnNavigation/LearnNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnNavigationApp.swift
+//  LearnNavigation
+//
+//  Created by Sajed Shaikh on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
